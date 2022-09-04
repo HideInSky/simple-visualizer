f0921877ddee2782d8338676f699717d6718175a
Linnn <linyucheng112@qq.com>
2022-09-03 22:17:10 -0500

    9/3 22:00

--- src/algorithms/dijkstra.js
+++ src/algorithms/dijkstra.js
@@ -1,47 +1,55 @@
+export function dijkstra(grid, startNode, finishNode) {
+  const visitedNodesInOrder = [];
+  startNode.distance = 0;
+  const unvisitedNodes = getAllNodes(grid); // get a grid copy
+  // array of nodes, 1 row 1->r, 2 row, 3...
+  // each node contains a dict of different properties.
 
-export function dijkstra (grid, startNode, finishNode){
+  while (!!unvisitedNodes.length) {
+    // 1. get nearest neighbors from unvisited nodes
+    sortNodesByDistance(unvisitedNodes);
+    const closestNode = unvisitedNodes.shift();
 
-    const visitedNodesInOrder = [];
-    startNode.distance = 0;
-    const unvisitedNodes = getAllNodes(grid); // get a grid copy
-    // array of nodes, 1 row 1->r, 2 row, 3...
-    // each node contains a dict of different properties.
-    
-    while (!!unvisitedNodes.length){
-        // 1. get nearest neighbors from unvisited nodes
-        sortNodesByDistance(unvisitedNodes);
-        const closestNode = unvisitedNodes.shift();
+    // check : if wall, skip the node;
+    if (closestNode.isWall) continue;
+    // ??? check : if infinity, all nodes are reached, return shortest path;
+    if (closestNode.distance === Infinity) return visitedNodesInOrder;
 
-        // check : if wall, skip the node;
-        if (closestNode.isWall) continue;
-        // check : if infinity, all nodes are reached, return shortest path;
-        if (closestNode.distance === Infinity) return visitedNodesInOrder;
+    // 2. set nearest neighbor as visited, and push it into vistedOrder
+    closestNode.isVisited = true;
+    visitedNodesInOrder.push(closestNode);
 
-        // 2. set nearest neighbor as visited, and push it into vistedOrder
-        closestNode.isVisited = true;
-        visitedNodesInOrder.push(closestNode);
+    // 3. return shortest path if reaching finishNode
+    if (closestNode === finishNode) return visitedNodesInOrder;
 
-        // 3. 
-        if (closestNode === finishNode) return visitedNodesInOrder;
-        updateUnvisitedNeighbors(closestNode, grid);
-    }
+    // 4. update neighbor distance
+    updateUnvisitedNeighbors(closestNode, grid);
+  }
 }
 
-function updateUnvisitedNeighbors(node, grid){
-    
+function getUnvisitedNeighbors(node, grid) {
+  const neighbors = [];
+  const { row, col } = node;
+  if (row > 0) neighbors.push(grid[row - 1][col]);
+  if (row < grid.length - 1) neighbors.push(grid[row + 1][col]);
+  if (col > 0) neighbors.push(grid[row][col - 1]);
+  if (row < grid[0].length - 1) neighbors.push(grid[row][col + 1]);
+  return neighbors.filter((neighbor) => !neighbor.isVisited);
+}
+
+function updateUnvisitedNeighbors(node, grid) {
+  const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
 }
 function getAllNodes(grid) {
-    const nodes = [];
-    for (const row of grid) {
-      for (const node of row) {
-        nodes.push(node);
-      }
+  const nodes = [];
+  for (const row of grid) {
+    for (const node of row) {
+      nodes.push(node);
     }
-    return nodes;
+  }
+  return nodes;
 }
 
-function sortNodesByDistance( unvisitedNodes){
-    unvisitedNodes.sort( (nodeA, nodeB) => nodeA.distance - nodeB.distance);
-      
+function sortNodesByDistance(unvisitedNodes) {
+  unvisitedNodes.sort((nodeA, nodeB) => nodeA.distance - nodeB.distance);
 }
-
