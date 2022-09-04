48e2069b0b091c221e0a496551d6aa84fab751fc
Linnn <linyucheng112@qq.com>
2022-09-04 01:43:31 -0500

    9/4 1:00

--- src/algorithms/dijkstra.js
+++ src/algorithms/dijkstra.js
@@ -39,6 +39,10 @@ function getUnvisitedNeighbors(node, grid) {
 
 function updateUnvisitedNeighbors(node, grid) {
   const unvisitedNeighbors = getUnvisitedNeighbors(node, grid);
+  for (const neighbor of unvisitedNeighbors) {
+    neighbor.distance = node.distance + 1;
+    neighbor.previousNode = node;
+  }
 }
 function getAllNodes(grid) {
   const nodes = [];
